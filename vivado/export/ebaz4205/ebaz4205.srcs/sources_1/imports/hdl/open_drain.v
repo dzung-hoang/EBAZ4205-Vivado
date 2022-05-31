1e99ca93eece58b89c2e7ab66d52c40bba071df2
wavelet27 <a3d35232@btinternet.com>
2022-05-31 19:55:29 +0100

    Changed FPGA pins driving LEDs to open drain.

---
+++ vivado/export/ebaz4205/ebaz4205.srcs/sources_1/imports/hdl/open_drain.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/24/2022 11:50:43 AM
+// Design Name: 
+// Module Name: open_drain
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module open_drain
+   #(parameter WIDTH=2)
+   (input  [WIDTH-1:0] Din,
+    output [WIDTH-1:0] ODout
+   );
+
+   genvar i;
+   generate
+      for (i=0; i<WIDTH; i=i+1) begin
+         OBUFT OBUFT_inst (
+            .O(ODout[i]),
+            .I(1'd0),
+            .T(!Din[i])
+         );
+      end
+   endgenerate
+
+endmodule
